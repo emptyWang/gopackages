7ce88b56d0e11b3597679b6deed4d6190325e264
wangzezhong <wangzezhong@zuoyebang.com>
2021-04-13 23:40:30 +0800

    first func

---
+++ base.go
@@ -0,0 +1,15 @@
+/**
+ * @Author: wangzezhong
+ * @Description:
+ * @File:  base
+ * @Version: 1.0.0
+ * @Date: 2021/4/13 11:38 PM
+ */
+
+package gopackages
+
+import "fmt"
+
+func SayFirst() {
+	fmt.Println("Hello World!")
+}
